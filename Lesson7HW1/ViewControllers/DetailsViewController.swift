d866ea1f9f4dcb25fed5f1729aabfafa006f6e86
Vladimir <vaksakalovbot@yandex.ru>
2020-06-09 12:29:49 +0300

    First Screen Done

---
+++ Lesson7HW1/ViewControllers/DetailsViewController.swift
@@ -0,0 +1,26 @@
+//
+//  DetailViewController.swift
+//  Lesson7HW1
+//
+//  Created by vaksakalov on 09.06.2020.
+//  Copyright © 2020 Vladimir. All rights reserved.
+//
+
+import UIKit
+
+class DetailsViewController: UIViewController {
+
+    @IBOutlet var phoneLabel: UILabel!
+    @IBOutlet var emailLabel: UILabel!
+    
+    var person: Person!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        phoneLabel.text = "Phone \(person.phone ?? "")"
+        emailLabel.text = "Email \(person.email ?? "")"
+        
+    }
+    
+}
